0846bd2eaabc6cf43a06ad1bb43117be39c0a3c0
Joppe2 <joppe.koers@codam.nl>
2022-02-16 11:41:54 +0100

    refactor:move code to api class

---
+++ src/api.ts
@@ -0,0 +1,85 @@
+import fetch from 'node-fetch'
+import fs from 'fs'
+import parameterAppend from 'url-parameter-append'
+
+interface Tokens {
+	clientUID: string
+	clientSecret: string
+}
+
+interface AccessToken {
+	access_token: string
+	token_type: string
+	expires_in: number
+	scope: string
+	created_at: number
+}
+
+export class API {
+	private _root: string
+	private _tokens: Tokens
+	private _accessToken: AccessToken | null
+	private _logging: boolean
+	private _accessTokenExpiry: number
+
+	constructor(tokenPath: string, logging: boolean = false, root = 'https://api.intra.42.fr') {
+		this._logging = logging
+		this._root = root
+		this._tokens = JSON.parse(fs.readFileSync(tokenPath).toString())
+		if (!this._tokens.clientUID.length || !this._tokens.clientSecret.length)
+			throw 'Token file invalid'
+		this._accessToken = null
+		this._accessTokenExpiry = -1
+	}
+
+	private async _updateToken() {
+		if (this._accessTokenExpiry > Date.now() + 60 * 1000)
+			return
+
+		const response = await fetch(`${this._root}/oauth/token`, {
+			method: 'POST',
+			body: `grant_type=client_credentials&client_id=${this._tokens.clientUID}&client_secret=${this._tokens.clientSecret}`,
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+			},
+		})
+		this._accessToken = await response.json()
+		this._accessTokenExpiry = this._accessToken!.created_at * 1000 + this._accessToken!.expires_in * 1000
+	}
+
+	async get(path: string, parameters?: { [key: string]: string | number }[]): Promise<any> {
+		await this._updateToken()
+		let requestPath = `${this._root}${path}`
+		if (parameters)
+			for (const key of parameters)
+				requestPath = parameterAppend(requestPath, key)
+
+		if (this._logging)
+			console.error('REQUEST', requestPath, parameters)
+		const response = await fetch(requestPath, {
+			headers: {
+				Authorization: `Bearer ${this._accessToken!.access_token}`,
+			}
+		})
+		return await response.json()
+	}
+
+	async getPaged(path: string, parameters?: { [key: string]: string | number }[]): Promise<any[]> {
+		let items: any[] = []
+
+		if (!parameters)
+			parameters = []
+		parameters['page[number]'] = '<placeholder>'
+		for (let i = 1; ; i++) {
+			for (const p of parameters)
+				if (p['page[number]'])
+					p['page[number]'] = i
+
+			const block = await this.get(path, parameters) // TODO: fix & and ?
+			if (block.length == 0)
+				break
+			items = items.concat(block)
+		}
+		return items
+	}
+}
--- src/app.ts
+++ src/app.ts
@@ -1,82 +1,32 @@
-import fetch from 'node-fetch'
 import fs from 'fs'
+import { API } from './api'
 
-interface Tokens {
-	clientUID: string
-	clientSecret: string
-}
+const Api: API = new API('./env/tokens.json', true)
 
-interface AccessToken {
-	access_token: string
-	token_type: string
-	expires_in: number
-	scope: string
-	created_at: number
-}
-const root = 'https://api.intra.42.fr'
-const settings = `?page[size]=10`
 const codamCampusID = 14
 const jkoersID = 66365
 const joppeID = 105119
 const netPracticeID = 2007
 
-async function getAccessToken(): Promise<AccessToken> {
-	const response = await fetch(`${root}/oauth/token`, {
-		method: 'POST',
-		body: `grant_type=client_credentials&client_id=${tokens.clientUID}&client_secret=${tokens.clientSecret}`,
-		headers: {
-			"Content-Type": "application/x-www-form-urlencoded",
-		},
-	})
-	return await response.json()
+
+async function getEvents() {
+	return await Api.get(`/v2/campus/${codamCampusID}/events`)
 }
 
-async function getEvents(accessToken: AccessToken) {
-	const response = await fetch(`${root}/v2/campus/${codamCampusID}/events${settings}`, {
-		headers: {
-			Authorization: `Bearer ${accessToken.access_token}`,
-		}
-	})
-	return await response.json()
+async function getProjects() {
+	return await Api.getPaged('/v2/projects/')
 }
 
-async function getProjects(accessToken: AccessToken) {
-	return await getInLoop(accessToken, '/v2/projects/')
+async function getProjectSubscribers(projectID: number): Promise<any[]> {
+	return await Api.getPaged(`/v2/projects/${projectID}/projects_users`, [{ 'filter[campus]': codamCampusID }])
 }
 
-async function get(accessToken: AccessToken, path: string): Promise<any> {
-	console.error('REQUEST', path)
-	const response = await fetch(`${root}${path}`, {
-		headers: {
-			Authorization: `Bearer ${accessToken.access_token}`,
-		}
-	})
-	return await response.json()
-}
-
-async function getInLoop(accessToken: AccessToken, path: string): Promise<any[]> {
-	let items: any[] = []
-
-	for (let i = 1; ; i++) {
-		const block = await get(accessToken, `${path}&page[number]=${i}`) // TODO: fix & and ?
-		if (block.length == 0)
-			break
-		items = items.concat(block)
-	}
-	return items
-}
-
-async function getProjectSubscribers(accessToken: AccessToken, projectID: number): Promise<any[]> {
-	return await getInLoop(accessToken, `/v2/projects/${projectID}/projects_users?filter[campus]=${codamCampusID}`)
-}
-
-const tokens: Tokens = JSON.parse(fs.readFileSync('tokens.json').toString());
 (async () => {
-	const accessToken: AccessToken = await getAccessToken()
 	const projectIDs = JSON.parse(fs.readFileSync('./data/projectIDs.json').toString())
+
 	for (const id in projectIDs) {
 		console.log(id)
-		const subs = await getProjectSubscribers(accessToken, projectIDs[id])
+		const subs = await getProjectSubscribers(projectIDs[id])
 		for (const sub of subs) {
 			if (sub.status != 'finished')
 				console.log(`${sub.user.login},${sub.status},${sub.marked}`)

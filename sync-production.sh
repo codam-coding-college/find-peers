4aabe4ed6c58b2fb2369ff260ed91b70842f5dc9
Joppe Koers <joppe@q42.nl>
2022-10-27 19:22:22 +0200

    feat: add sync

---
+++ sync-production.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run this script to sync the local repo to production
+
+set -o strace # Print commands as they are executed
+set -o errexit # Exit on error
+
+SSH="ssh find-peers"
+
+# If there are local changes, this will fail. It should be like that
+$SSH 'cd /root/find-peers && git pull origin main'
+$SSH 'docker build -t find-peers /root/find-peers'
+
+$SSH 'docker stop find-peers'
+$SSH 'docker rm find-peers'
+$SSH 'docker run -v $PWD/database:/app/database -d -p 80:8080 --name find-peers find-peers'
+
+$SSH 'docker logs -f find-peers'
+# Press Ctrl+C to stop following the logs

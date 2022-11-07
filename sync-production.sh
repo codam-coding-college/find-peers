35ffafa78edb47c126ffb040e062cbf18114dd93
Joppe Koers <joppe@q42.nl>
2022-11-07 19:46:28 +0100

    feat: assert build success before sync to prod

--- sync-production.sh
+++ sync-production.sh
@@ -5,6 +5,9 @@
 set -o xtrace # Print commands as they are executed
 set -o errexit # Exit on error
 
+# Makiig sure that we can acctually build
+docker build -t find-peers .
+
 SSH="ssh find-peers"
 
 # If there are local changes, this will fail. It should be like that

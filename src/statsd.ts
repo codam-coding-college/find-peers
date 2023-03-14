a3fb9e882a898049f9acf8ce3be1e2eacfd80484
Joppe Koers <joppe@joppekoers.nl>
2023-03-14 08:26:25 +0100

    normalize tags

--- src/statsd.ts
+++ src/statsd.ts
@@ -3,7 +3,7 @@ import { env } from './env'
 
 const client = new StatsDObj({
 	port: 8125,
-	host: 'localhost',
+	host: 'datadog-agent', // TODO use env
 	errorHandler: console.error,
 })
 
@@ -11,15 +11,29 @@ const stats = {
 	visits: Object.keys(env.campusIDs) as unknown as keyof typeof env.campusIDs,
 } as const
 
+for (const stat of Object.keys(stats)) {
+	if (stat != stat.toLowerCase())
+		throw new Error(`StatsD stat "${stat}" must be lowercase`)
+}
+
 export namespace StatsD {
 	export type Stat = keyof typeof stats
 	export type Tag<T extends Stat> = typeof stats[T]
 
 	export function increment(stat: Stat, tag?: Tag<Stat>): void {
-		if (tag) {
-			client.increment(stat, [tag])
-		} else {
-			client.increment(stat)
+		if (!tag) {
+			return client.increment(stat)
 		}
+
+		// Datadog only allows some characters in tags
+		const tagNormalized = tag
+			.normalize("NFD")
+			.replace(/[\u0300-\u036f]/g, "")
+			.replace(/ |-/g, '_')
+			.toLowerCase()
+		console.log('tag', tag, tagNormalized)
+		client.increment(stat, [tagNormalized])
+		client.increment(stat, ['paris'])
+
 	}
 }
